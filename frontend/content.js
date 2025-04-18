90bb9e75fffe564c8e8e3456df76919e85f94d02
Favourfisayo <Favourfisayo5@gmail.com>
2025-04-18 15:49:09 +0100

    Backend deployed with Render

--- frontend/content.js
+++ frontend/content.js
@@ -12,7 +12,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       return;
     }
 
-    fetch(`http://localhost:3000/transcript?videoId=${videoId}`)
+    fetch(`https://youtube-notes-backend.onrender.com/transcript?videoId=${videoId}`)
       .then(res => res.json())
       .then(data => {
         console.log("Content: fetched data", data);
